020d9df511542c2457880e5933e8dc1b1f2049b8
l1xnan@qq.com <l1xnan@qq.com>
2023-10-11 20:15:34 +0800

    use material-react-table

--- src/Dataset.tsx
+++ src/Dataset.tsx
@@ -1,6 +1,10 @@
 import { MantineReactTable, useMantineReactTable } from "mantine-react-table";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
 
-export default function Dataset({ data, columns }: any) {
+function Dataset1({ data, columns }: any) {
   //should be memoized or stable
 
   const table = useMantineReactTable({
@@ -9,4 +13,47 @@ export default function Dataset({ data, columns }: any) {
   });
 
   return <MantineReactTable table={table} />;
+}
+export default function Dataset({ data, columns }: any) {
+  //should be memoized or stable
+
+  const table = useMaterialReactTable({
+    columns,
+    data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+  });
+
+  return (
+    <MaterialReactTable
+      table={table}
+      initialState={{ density: "compact" }}
+      enableColumnActions={true}
+      enableColumnFilters={true}
+      enablePagination={true}
+      enableSorting={true}
+      enableBottomToolbar={true}
+      enableTopToolbar={true}
+      muiTableBodyRowProps={{ hover: true }}
+      muiTableProps={
+        {
+          // sx: {
+          //   border: '1px solid rgba(81, 81, 81, 1)',
+          // },
+        }
+      }
+      muiTableHeadCellProps={
+        {
+          // sx: {
+          //   border: '1px solid rgba(81, 81, 81, 1)',
+          // },
+        }
+      }
+      muiTableBodyCellProps={
+        {
+          // sx: {
+          //   border: '1px solid rgba(81, 81, 81, 1)',
+          // },
+        }
+      }
+    />
+  );
 }
