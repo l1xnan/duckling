2450f8af4b54a5735fc011d6eedb48854a8c0cec
l1xnan@qq.com <l1xnan@qq.com>
2023-10-12 20:19:35 +0800

    change scrollbar style

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,43 @@ const theme = createTheme({
     fontFamily: "Consolas",
     fontSize: 14,
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          scrollbarColor: "#6b6b6b #2b2b2b",
+          "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+            // backgroundColor: "#2b2b2b",
+            backgroundColor: "#fcfcfc",
+            width: "0.6em",
+            height: "0.6em",
+          },
+          "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+            borderRadius: 0,
+            // backgroundColor: "#6b6b6b",
+            backgroundColor: "#8b8b8b",
+            minHeight: 12,
+            // border: "2px solid #2b2b2b",
+          },
+          "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus":
+            {
+              backgroundColor: "#959595",
+            },
+          "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active":
+            {
+              backgroundColor: "#959595",
+            },
+          "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
+            {
+              backgroundColor: "#959595",
+            },
+          "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+            backgroundColor: "#2b2b2b",
+          },
+        },
+      },
+    },
+  },
 });
 
 const DialogButton = () => {
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
