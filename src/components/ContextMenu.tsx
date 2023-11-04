e6863810942a9d9c95d04d2c64b5f15d8a8cfae1
l1xnan <l1xnan@qq.com>
2023-11-04 19:26:07 +0800

    refactor: remove unreachable code

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -8,29 +8,18 @@ import {
 
 import { isDarkTheme } from "@/utils";
 
-export const ContextMenu = styled((props: MenuProps) => (
-  <Menu
-    // elevation={0}
-    // anchorOrigin={{
-    //   vertical: "bottom",
-    //   horizontal: "left",
-    // }}
-    // transformOrigin={{
-    //   vertical: "top",
-    //   horizontal: "left",
-    // }}
-    {...props}
-  />
-))(({ theme }) => ({
-  "& .MuiPaper-root": {
-    // borderRadius: 0,
-    backgroundColor: isDarkTheme(theme) ? "#2b2d30" : "#ffffff",
-    color: !isDarkTheme(theme) ? "rgb(55, 65, 81)" : theme.palette.grey[300],
-    border: isDarkTheme(theme) ? "1px solid #43454a" : "1px solid #b9bdc9",
-    boxShadow:
-      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1)  10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-  },
-}));
+export const ContextMenu = styled((props: MenuProps) => <Menu {...props} />)(
+  ({ theme }) => ({
+    "& .MuiPaper-root": {
+      // borderRadius: 0,
+      backgroundColor: isDarkTheme(theme) ? "#2b2d30" : "#ffffff",
+      color: !isDarkTheme(theme) ? "rgb(55, 65, 81)" : theme.palette.grey[300],
+      border: isDarkTheme(theme) ? "1px solid #43454a" : "1px solid #b9bdc9",
+      boxShadow:
+        "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1)  10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    },
+  })
+);
 
 export const ContextMenuItem = styled((props: MenuItemProps) => (
   <MenuItem dense {...props} />
