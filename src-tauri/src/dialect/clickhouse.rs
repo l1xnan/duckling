f5385620ca6af93dd6582a22b9e89dbaaca5b509
l1xnan <l1xnan@qq.com>
2024-01-16 21:05:44 +0800

    update clickhouse

--- src-tauri/src/dialect/clickhouse.rs
+++ src-tauri/src/dialect/clickhouse.rs
@@ -19,10 +19,7 @@ impl ClickhouseDialect {
   fn get_url(&self) -> String {
     format!(
       "tcp://{}:{}@{}:{}/clicks?compression=lz4&ping_timeout=42ms",
-      self.username,
-      self.password,
-      self.host,
-      self.port,
+      self.username, self.password, self.host, self.port,
     )
   }
 }
@@ -35,7 +32,18 @@ struct Table {
 
 impl Dialect for ClickhouseDialect {
   async fn get_db(&self) -> Option<TreeNode> {
-    // let pool = Pool::new(database_url);
+    let url = self.get_url();
+    let pool = Pool::new(url);
+    let mut client = pool.get_handle();
+    let block = client
+      .query("select database, name from system.tables")
+      .fetch_all()
+      .await?;
+    for row in block.rows() {
+      let database: String = row.get("database")?;
+      let name: String = row.get("name")?;
+      println!("tables: {}.{}", database, name);
+    }
     None
   }
 }
