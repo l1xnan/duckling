8d202ffc0b5a1fdffee5a910d9643eb3c6a1b1e5
l1xnan <l1xnan@qq.com>
2024-02-06 01:06:22 +0800

    add tailwind-scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,5 +74,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
 } satisfies Config;
