5268568b08524c6a1978680168cc27969bfbac49
l1xnan <l1xnan@qq.com>
2024-02-23 23:36:15 +0800

    type convert

--- src-tauri/src/dialect/sqlite.rs
+++ src-tauri/src/dialect/sqlite.rs
@@ -1,9 +1,11 @@
+use std::convert::From;
 use std::sync::Arc;
 
 use arrow::array::*;
 use arrow::datatypes::*;
 use arrow::datatypes::{DataType, Field, Schema};
 use async_trait::async_trait;
+use futures_util::StreamExt;
 use rusqlite::types::Value;
 use rusqlite::Connection;
 
@@ -109,6 +111,71 @@ impl SqliteDialect {
   async fn get_schema(&self) -> Vec<Table> {
     vec![]
   }
+
+  fn fetch_all(&self, sql: &str) -> anyhow::Result<ArrowData> {
+    let conn = Connection::open(&self.path)?;
+    let mut stmt = conn.prepare(sql)?;
+
+    let mut fields = vec![];
+    let k = stmt.column_count();
+    let mut titles = vec![];
+    for col in stmt.columns() {
+      titles.push(Title {
+        name: col.name().to_string(),
+        r#type: col.decl_type().unwrap_or_default().to_string(),
+      });
+      let typ = if let Some(decl_type) = col.decl_type() {
+        match decl_type {
+          "INTEGER" => DataType::Int64,
+          "REAL" => DataType::Float64,
+          "NUMERIC" => DataType::Utf8,
+          "BOOLEAN" => DataType::Boolean,
+          "DATE" => DataType::Utf8,
+          "DATETIME" => DataType::Utf8,
+          "TIME" => DataType::Utf8,
+          "BLOB" => DataType::Binary,
+          "NULL" => DataType::Null,
+          _ => DataType::Utf8,
+        }
+      } else {
+        DataType::Null
+      };
+      let field = Field::new(col.name(), typ, true);
+      fields.push(field);
+      println!("{:?} {:?}", col.name(), col.decl_type())
+    }
+
+    let schema = Schema::new(fields);
+
+    let mut rows = stmt.query([])?;
+    let mut tables: Vec<Vec<Value>> = (0..k).map(|_| vec![]).collect();
+    while let Some(row) = rows.next()? {
+      for i in 0..k {
+        let val = row.get::<_, Value>(i).unwrap();
+        tables[i].push(val);
+      }
+    }
+
+    let mut arrs = vec![];
+    for (col, title) in tables.iter().zip(titles) {
+      let arr: ArrayRef = match title.r#type.as_str() {
+        "INTEGER" => Arc::new(Int64Array::from(convert_to_i64s(col))),
+        "REAL" => Arc::new(Float64Array::from(convert_to_f64s(col))),
+        "NUMERIC" => Arc::new(StringArray::from(convert_to_strings(col))),
+        "DATE" | "DATETIME" => Arc::new(StringArray::from(convert_to_strings(col))),
+        "TIME" => Arc::new(StringArray::from(convert_to_strings(col))),
+        "BOOLEAN" => Arc::new(StringArray::from(convert_to_strings(col))),
+        _ => Arc::new(StringArray::from(convert_to_strings(col))),
+      };
+      arrs.push(arr);
+    }
+    let batch = RecordBatch::try_new(Arc::new(schema), arrs)?;
+
+    Ok(ArrowData {
+      total_count: batch.num_rows(),
+      preview: serialize_preview(&batch)?,
+    })
+  }
 }
 
 pub async fn get_tables(path: &str) -> anyhow::Result<Vec<Table>> {
@@ -157,4 +224,44 @@ pub fn convert_arrow(value: &Value, typ: &str) -> ArrayRef {
       _ => Arc::new(StringArray::from(vec![None::<String>])) as ArrayRef,
     },
   }
+}
+
+pub fn convert_to_string(value: &Value) -> Option<String> {
+  match value {
+    Value::Integer(i) => Some(i.to_string()),
+    Value::Real(f) => Some(f.to_string()),
+    Value::Text(s) => Some(s.clone()),
+    Value::Blob(b) => String::from_utf8(b.clone()).ok(),
+    Value::Null => None::<String>,
+  }
+}
+
+pub fn convert_to_i64(value: &Value) -> Option<i64> {
+  match value {
+    Value::Integer(i) => Some(*i),
+    Value::Real(f) => Some(*f as i64),
+    Value::Text(s) => s.parse::<i64>().ok(),
+    _ => None::<i64>,
+  }
+}
+
+pub fn convert_to_f64(value: &Value) -> Option<f64> {
+  match value {
+    Value::Integer(i) => i.to_string().parse::<f64>().ok(),
+    Value::Real(f) => Some(*f),
+    Value::Text(s) => s.parse::<f64>().ok(),
+    _ => None::<f64>,
+  }
+}
+
+pub fn convert_to_strings(values: &[Value]) -> Vec<Option<String>> {
+  values.iter().map(|v| convert_to_string(v)).collect()
+}
+
+pub fn convert_to_i64s(values: &[Value]) -> Vec<Option<i64>> {
+  values.iter().map(|v| convert_to_i64(v)).collect()
+}
+
+pub fn convert_to_f64s(values: &[Value]) -> Vec<Option<f64>> {
+  values.iter().map(|v| convert_to_f64(v)).collect()
 }
