027940a3798cac0502e1e1acd562b91e52194f78
l1xnan <l1xnan@qq.com>
2024-02-24 00:00:23 +0800

    feat: support mysql

--- src-tauri/src/dialect/mysql.rs
+++ src-tauri/src/dialect/mysql.rs
@@ -1,16 +1,11 @@
-use std::sync::Arc;
-
 use arrow::array::*;
-use arrow::datatypes::*;
 use async_trait::async_trait;
-use futures_util::TryStreamExt;
-use sqlx::mysql::{MySql, MySqlPool, MySqlRow, MySqlTypeInfo};
-use sqlx::types::chrono::{NaiveDate, NaiveDateTime};
-use sqlx::{Column, ConnectOptions, Connection, Database, Executor, Row, TypeInfo};
+use mysql::prelude::*;
+use mysql::*;
 
-use crate::api::{serialize_preview, ArrowData};
+use crate::api::ArrowData;
 use crate::dialect::{Dialect, TreeNode};
-use crate::utils::{build_tree, date_to_days, new_conn, Table};
+use crate::utils::{build_tree, Table};
 
 #[derive(Debug, Default)]
 pub struct MySqlDialect {
@@ -38,40 +33,7 @@ impl Dialect for MySqlDialect {
   }
 
   async fn query(&self, sql: &str, limit: usize, offset: usize) -> anyhow::Result<ArrowData> {
-    // let pool = MySqlPool::connect(&self.path).await?;
-    let mut conn = new_conn::<MySql>(&self.get_url()).await?;
-
-    let info = conn.describe(sql).await?;
-    let columns = info.columns();
-    println!("{:?}", columns);
-    let mut fields = vec![];
-    for (k, col) in columns.iter().enumerate() {
-      let typ = convert_type(col.type_info());
-      let field = Field::new(col.name(), typ, true);
-      fields.push(field);
-    }
-    let mut cursor = conn.fetch(sql);
-    let schema = Schema::new(fields);
-
-    let mut i = 0;
-    let mut batchs = vec![];
-    while let Some(row) = cursor.try_next().await? {
-      let mut arrs = vec![];
-      for (k, col) in columns.iter().enumerate() {
-        let r = convert_row(&row, k, col.type_info().name());
-        arrs.push(r);
-      }
-      let batch = RecordBatch::try_new(Arc::new(schema.clone()), arrs)?;
-      batchs.push(batch);
-      i += 1;
-    }
-
-    let batch = arrow::compute::concat_batches(&Arc::new(schema), &batchs)?;
-
-    Ok(ArrowData {
-      total_count: batch.num_rows(),
-      preview: serialize_preview(&batch)?,
-    })
+    unimplemented!()
   }
 }
 
@@ -93,69 +55,32 @@ impl MySqlDialect {
 }
 
 pub async fn get_tables(url: &str) -> anyhow::Result<Vec<Table>> {
-  let pool = MySqlPool::connect(url).await?;
+  let pool = Pool::new(url)?;
+
+  let mut conn = pool.get_conn()?;
 
   let sql = r#"
   select 
     TABLE_SCHEMA as table_schema, 
-    TABLE_TYPE as table_type, 
     TABLE_NAME as table_name,
+    TABLE_TYPE as table_type,
     if(TABLE_TYPE='BASE TABLE', 'table', 'view') as type
   from information_schema.tables
   "#;
-  let rows = sqlx::query(sql).fetch_all(&pool).await?;
+  let mut tables = vec![];
+  let mut result = conn.query_iter(sql)?;
+  while let Some(result_set) = result.iter() {
+    for row in result_set {
+      if let Ok(r) = row {
+        tables.push(Table {
+          table_schema: r.get::<String, _>(0).unwrap(),
+          table_name: r.get::<String, _>(1).unwrap(),
+          table_type: r.get::<String, _>(2).unwrap(),
+          r#type: r.get::<String, _>(2).unwrap(),
+        });
+      }
+    }
+  }
 
-  let tables: Vec<Table> = rows
-    .iter()
-    .map(|r| Table {
-      table_name: r.get::<String, _>("table_name"),
-      table_type: r.get::<String, _>("table_type"),
-      table_schema: r.get::<String, _>("table_schema"),
-      r#type: r.get::<String, _>("type"),
-    })
-    .collect();
   Ok(tables)
-}
-
-fn convert_type(col_type: &MySqlTypeInfo) -> DataType {
-  match col_type.name() {
-    "INTEGER" => DataType::Int64,
-    "REAL" | "NUMERIC" => DataType::Float64,
-    "BOOLEAN" => DataType::Boolean,
-    "DATE" => DataType::Date32,
-    "DATETIME" => DataType::Date64,
-    "TIME" => DataType::Utf8,
-    "BLOB" => DataType::Binary,
-    "NULL" => DataType::Null,
-    _ => DataType::Utf8,
-  }
-}
-
-fn convert_row(row: &MySqlRow, k: usize, type_name: &str) -> ArrayRef {
-  match type_name {
-    "INTEGER" => Arc::new(Int64Array::from(vec![row.try_get::<i64, _>(k).ok()])) as ArrayRef,
-    "REAL" | "NUMERIC" => {
-      Arc::new(Float64Array::from(vec![row.try_get::<f64, _>(k).ok()])) as ArrayRef
-    }
-    "BOOLEAN" => Arc::new(BooleanArray::from(vec![row.try_get::<bool, _>(k).ok()])) as ArrayRef,
-    "DATE" => {
-      let val = row
-        .try_get::<NaiveDate, _>(k)
-        .ok()
-        .map(|d| date_to_days(&d));
-      Arc::new(Date32Array::from(vec![val])) as ArrayRef
-    }
-    "DATETIME" => {
-      let val = row
-        .try_get::<NaiveDateTime, _>(k)
-        .ok()
-        .map(|t| t.timestamp() * 1000);
-      Arc::new(Date64Array::from(vec![val])) as ArrayRef
-    }
-    "TEXT" => Arc::new(StringArray::from(vec![row.try_get::<&str, _>(k).ok()])) as ArrayRef,
-    "BLOB" => Arc::new(BinaryArray::from_opt_vec(vec![row
-      .try_get::<&[u8], _>(k)
-      .ok()])) as ArrayRef,
-    _ => Arc::new(StringArray::from(vec![row.try_get::<&str, _>(k).ok()])) as ArrayRef,
-  }
 }
