4712a8f192b5822b059d3fcc17d09a37f5b0f94c
l1xnan <l1xnan@qq.com>
2024-03-11 17:22:48 +0800

    feat: add schema search

--- src/components/Schema.tsx
+++ src/components/Schema.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue } from 'jotai';
-import { Loader2Icon } from 'lucide-react';
+import { Loader2Icon, RefreshCw, Search } from 'lucide-react';
 import {
   ReactNode,
   Suspense,
@@ -19,8 +19,11 @@ import {
   activeTabAtom,
   getDatabase,
 } from '@/stores/tabs';
+import { isEmpty } from '@/utils';
 
 import { SimpleTable } from './CanvasTable';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 export const Loading = ({ className }: { className?: string }) => {
   return (
@@ -63,29 +66,63 @@ export function DatabaseSchema({ context }: { context: SchemaContextType }) {
 
   const [data, setData] = useState<unknown[]>([]);
 
+  const handleQuery = async () => {
+    try {
+      setLoading(true);
+      const { data } = await showSchema(context.schema as string, db.config);
+      setData(data);
+    } catch (error) {
+      toast.error((error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     if (currentTab?.id == context.id) {
-      (async () => {
-        try {
-          setLoading(true);
-          const { data } = await showSchema(
-            context.schema as string,
-            db.config,
-          );
-          setData(data);
-        } catch (error) {
-          toast.error((error as Error).message);
-        } finally {
-          setLoading(false);
-        }
-      })();
+      handleQuery();
     }
   }, []);
+  const [search, setSearch] = useState('');
 
   return (
     <div className="h-full">
+      <div className="h-[32px] flex flex-row justify-between">
+        <div className="relative flex-1 ">
+          <Search className="absolute left-2 top-2 h-4 w-4 text-muted-foreground" />
+          <Input
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            placeholder="Search"
+            className="w-full h-full pl-8 py-0.5 text-xs focus-visible:ring-0 shadow-none rounded-none border-t-0 border-b transition-none"
+          />
+        </div>
+
+        <Button variant="link" size="icon" onClick={handleQuery}>
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
       <Suspense fallback={<Loading />}>
-        {loading ? <Loading /> : <SimpleTable data={data ?? []} />}
+        {loading ? (
+          <Loading />
+        ) : (
+          <SimpleTable
+            data={
+              data.filter((item) => {
+                if (isEmpty(search)) {
+                  return true;
+                }
+                if (item?.name) {
+                  JSON.stringify(Object.values(item));
+                  return item?.name?.includes(search);
+                }
+                return true;
+              }) ?? []
+            }
+          />
+        )}
       </Suspense>
     </div>
   );
