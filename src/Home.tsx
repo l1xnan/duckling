6ffd4cb5db2b5dcea8acf74af3592637d552daf4
l1xnan <l1xnan@qq.com>
2024-03-23 00:18:40 +0800

    add sidebar icon

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,13 @@
 import { useAtom } from 'jotai/react';
 
 import { Content, Sidebar as SidebarWrapper } from '@/components/Layout';
+import { Button } from '@/components/ui/button.tsx';
 import { useResize } from '@/hooks';
 import classes from '@/hooks/resize.module.css';
 import { Main } from '@/pages/main';
 import Sidebar from '@/pages/sidebar';
 import { sizeAtom } from '@/stores/app';
+import { DatabaseIcon, FolderIcon, HistoryIcon } from 'lucide-react';
 import { RefObject } from 'react';
 
 function Home() {
@@ -19,10 +21,21 @@ function Home() {
 
   return (
     <div className="h-full max-h-screen p-0 m-0">
+      <div className="h-full left-0 top-0 absolute flex flex-col w-8 items-center border-r">
+        <Button variant="ghost" size="icon" className="rounded">
+          <DatabaseIcon className="h-4 w-4" />
+        </Button>
+        <Button variant="ghost" size="icon" className="rounded">
+          <FolderIcon className="h-4 w-4" />
+        </Button>
+        <Button variant="ghost" size="icon" className="rounded">
+          <HistoryIcon className="h-4 w-4" />
+        </Button>
+      </div>
       <div
         ref={targetRefLeft as RefObject<HTMLDivElement>}
-        className="h-full left-0 top-0 absolute flex"
-        style={{ width: sizeLeft }}
+        className="h-full left-8 top-0 absolute flex"
+        style={{ width: sizeLeft - 30 }}
       >
         <SidebarWrapper>
           <Sidebar />
