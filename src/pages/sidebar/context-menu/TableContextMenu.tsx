63e5e6588a097a5fa37052d1dfa16dd00678de62
l1xnan <l1xnan@qq.com>
2024-03-27 20:36:46 +0800

    delete add dialog

--- src/pages/sidebar/context-menu/TableContextMenu.tsx
+++ src/pages/sidebar/context-menu/TableContextMenu.tsx
@@ -16,6 +16,19 @@ import { TableContextType, useTabsStore } from '@/stores/tabs';
 import { TreeNode } from '@/types';
 import { useAtom } from 'jotai';
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
 export function TableContextMenu({
   children,
   node,
@@ -30,101 +43,131 @@ export function TableContextMenu({
     e.stopPropagation();
     await dropTable(node.path, db.config as DialectConfig);
   };
+
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>{children}</ContextMenuTrigger>
-      <ContextMenuContent className="w-64">
-        <ContextMenuItem
-          onClick={async (e) => {
+    <AlertDialog
+      onOpenChange={(s) => {
+        console.log('alert', s);
+      }}
+    >
+      <ContextMenu>
+        <ContextMenuTrigger>{children}</ContextMenuTrigger>
+        <ContextMenuContent
+          className="w-64"
+          onClick={(e) => {
             e.stopPropagation();
-            console.log(node);
-            const item: TableContextType = {
-              id: nanoid(),
-              dbId: db.id,
-              tableId: node.path,
-              displayName: node?.name as string,
-              type: 'column',
-            };
-            updateTab!(item);
           }}
         >
-          Show columns
-        </ContextMenuItem>
-        {db.dialect == 'folder' ? (
-          <>
-            <ContextMenuItem
-              onClick={async (e) => {
-                e.stopPropagation();
-                console.log(node);
-                const item: TableContextType = {
-                  id: nanoid(),
-                  dbId: db.id,
-                  tableId: node.path,
-                  tableName:
-                    node.type == 'path'
-                      ? `read_parquet('${node.path}/*.parquet', union_by_name=true, filename=true)`
-                      : undefined,
-                  displayName: node?.name as string,
-                  type: 'table',
-                };
-                updateTab!(item);
-              }}
-            >
-              Show *.parquet
-            </ContextMenuItem>
-            <ContextMenuItem
-              onClick={async (e) => {
-                e.stopPropagation();
-                console.log(node);
-                const item: TableContextType = {
-                  id: nanoid(),
-                  dbId: db.id,
-                  tableId: node.path,
-                  tableName:
-                    node.type == 'path'
-                      ? `read_csv('${node.path}/*.csv', union_by_name=true, filename=true)`
-                      : undefined,
-                  displayName: node?.name as string,
-                  type: 'table',
-                };
-                updateTab!(item);
-              }}
-            >
-              Show *.csv
-            </ContextMenuItem>
-            <ContextMenuSeparator />
-            <ContextMenuItem
-              onClick={async (e) => {
-                e.stopPropagation();
-                console.log(node);
-                const item: TableContextType = {
-                  id: nanoid(),
-                  dbId: db.id,
-                  tableId: node.path,
-                  displayName: node?.name as string,
-                  type: 'table',
-                };
+          <ContextMenuItem
+            onClick={async (e) => {
+              e.stopPropagation();
+              console.log(node);
+              const item: TableContextType = {
+                id: nanoid(),
+                dbId: db.id,
+                tableId: node.path,
+                displayName: node?.name as string,
+                type: 'column',
+              };
+              updateTab!(item);
+            }}
+          >
+            Show columns
+          </ContextMenuItem>
+          {db.dialect == 'folder' ? (
+            <>
+              <ContextMenuItem
+                onClick={async (e) => {
+                  e.stopPropagation();
+                  console.log(node);
+                  const item: TableContextType = {
+                    id: nanoid(),
+                    dbId: db.id,
+                    tableId: node.path,
+                    tableName:
+                      node.type == 'path'
+                        ? `read_parquet('${node.path}/*.parquet', union_by_name=true, filename=true)`
+                        : undefined,
+                    displayName: node?.name as string,
+                    type: 'table',
+                  };
+                  updateTab!(item);
+                }}
+              >
+                Show *.parquet
+              </ContextMenuItem>
+              <ContextMenuItem
+                onClick={async (e) => {
+                  e.stopPropagation();
+                  console.log(node);
+                  const item: TableContextType = {
+                    id: nanoid(),
+                    dbId: db.id,
+                    tableId: node.path,
+                    tableName:
+                      node.type == 'path'
+                        ? `read_csv('${node.path}/*.csv', union_by_name=true, filename=true)`
+                        : undefined,
+                    displayName: node?.name as string,
+                    type: 'table',
+                  };
+                  updateTab!(item);
+                }}
+              >
+                Show *.csv
+              </ContextMenuItem>
+              <ContextMenuSeparator />
+              <ContextMenuItem
+                onClick={async (e) => {
+                  e.stopPropagation();
+                  console.log(node);
+                  const item: TableContextType = {
+                    id: nanoid(),
+                    dbId: db.id,
+                    tableId: node.path,
+                    displayName: node?.name as string,
+                    type: 'table',
+                  };
 
-                setSearch({ open: true, item });
-              }}
-            >
-              Search
-            </ContextMenuItem>
-          </>
-        ) : null}
+                  setSearch({ open: true, item });
+                }}
+              >
+                Search
+              </ContextMenuItem>
+            </>
+          ) : null}
 
-        <ContextMenuSeparator />
-        <ContextMenuItem onClick={handleDropTable}>Delete</ContextMenuItem>
-        <ContextMenuSeparator />
-        <ContextMenuItem
-          onClick={async (e) => {
-            e.stopPropagation();
-            await writeText(node.path);
-          }}
-        >
-          Copy
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+          <ContextMenuSeparator />
+
+          <AlertDialogTrigger asChild>
+            <ContextMenuItem>Delete</ContextMenuItem>
+          </AlertDialogTrigger>
+
+          <ContextMenuSeparator />
+          <ContextMenuItem
+            onClick={async (e) => {
+              e.stopPropagation();
+              await writeText(node.path);
+            }}
+          >
+            Copy
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Confirm deletion of table: <code>{node.path}</code>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild onClick={handleDropTable}>
+            <Button variant="destructive">Yes</Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
