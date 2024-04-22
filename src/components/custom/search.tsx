b5b8a9acdc27e90ffc0115c40cd75c83d8c92dd1
l1xnan <l1xnan@qq.com>
2024-04-22 18:17:32 +0800

    update

--- src/components/custom/search.tsx
+++ src/components/custom/search.tsx
@@ -6,14 +6,17 @@ import React from 'react';
 export const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
     return (
-      <div className={cn('relative h-8 border-b', className)}>
-        <Search
-          className={'absolute left-2 top-2 h-4 w-4 text-muted-foreground'}
-        />
+      <div
+        className={cn(
+          'relative flex items-center justify-start h-8 border-b',
+          className,
+        )}
+      >
+        <Search className={'size-4 ml-2 text-muted-foreground'} />
         <Input
           ref={ref}
           placeholder="Search"
-          className="h-8 pl-8 py-0.5 text-xs focus-visible:ring-0 shadow-none rounded-none border-none transition-none"
+          className="h-8 pl-2 py-0.5 text-xs focus-visible:ring-0 shadow-none rounded-none border-none transition-none"
           {...props}
         />
       </div>
