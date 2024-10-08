10c01cae9206f1c000c2a3f5140699f29c25b4a3
l1xnan <lixnan@hotmail.com>
2024-10-08 14:02:44 +0800

    chore: update tauri 2.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -31,4 +31,13 @@ export default defineConfig(async ({ mode }) => ({
   // 3. to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ['VITE_', 'TAURI_'],
-}));
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+});
