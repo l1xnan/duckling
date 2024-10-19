46e2a899ba4092bc70e272ecf494a9057db3a324
l1xnan <l1xnan@qq.com>
2024-10-19 16:44:47 +0800

    chore: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
 import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
-export default defineConfig(async ({ mode }) => ({
+export default defineConfig({
   plugins: [
     react({
       babel: {
