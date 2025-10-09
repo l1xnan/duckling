edb6c1b4af9b01818634c1e179c72820b86a3782
l1xnan <lixnan@hotmail.com>
2025-10-09 13:24:54 +0800

    publish version 0.0.50

--- src/components/custom/TreeView.tsx
+++ src/components/custom/TreeView.tsx
@@ -74,9 +74,9 @@ export function ContextNode({
 }
 
 interface NodeProps {
-    tree: TreeInstance<Node3Type>;
-    item: ItemInstance<Node3Type>;
-    style: React.CSSProperties;
+  tree: TreeInstance<Node3Type>;
+  item: ItemInstance<Node3Type>;
+  style: React.CSSProperties;
 }
 const Node = ({ tree, item, style }: NodeProps) => {
   try {
@@ -86,80 +86,80 @@ const Node = ({ tree, item, style }: NodeProps) => {
     console.warn(error);
     return null;
   }
-    const { onClick, ...props } = item.getProps();
-    const rowAttrs: React.HTMLAttributes<any> = {
-      role: 'treeitem',
-      'aria-expanded': item.isExpanded(),
-    };
-    const node = item.getItemData();
-    const { displayName, path, name, icon } = node?.data ?? {};
-    const level = item.getItemMeta().level;
+  const { onClick, ...props } = item.getProps();
+  const rowAttrs: React.HTMLAttributes<any> = {
+    role: 'treeitem',
+    'aria-expanded': item.isExpanded(),
+  };
+  const node = item.getItemData();
+  const { displayName, path, name, icon } = node?.data ?? {};
+  const level = item.getItemMeta().level;
 
-    const isRoot = level === 0;
-    return (
-      <div style={style} className="w-full h-6" ref={item.registerElement}>
-        <ContextNode data={node?.data} isRoot={isRoot}>
+  const isRoot = level === 0;
+  return (
+    <div style={style} className="w-full h-6" ref={item.registerElement}>
+      <ContextNode data={node?.data} isRoot={isRoot}>
+        <div
+          key={item.getId()}
+          onDoubleClick={(e) => {
+            onClick(e);
+            props.onDoubleClick?.();
+            props.onSelect?.();
+          }}
+          onClick={() => {
+            tree.setSelectedItems([item.getItemMeta().itemId]);
+            props.onSelect?.();
+          }}
+          {...rowAttrs}
+          {...props}
+          style={{ paddingLeft: `${level * 16}px` }}
+          className={cn(
+            'group',
+            'treeitem',
+            'relative',
+            'transition-colors',
+            'flex items-center gap-1',
+            'text-sm',
+            'cursor-pointer',
+            'select-none',
+            'text-foreground',
+            'hover:bg-accent',
+            'aria-selected:bg-selection',
+            'aria-selected:hover:bg-selection',
+            'h-6',
+          )}
+        >
+          {item.isFolder() ? (
+            <ChevronRight
+              onClick={(e) => {
+                onClick?.(e);
+                item.onSelect?.();
+              }}
+              className={cn(
+                'text-foreground-muted',
+                'transition-transform duration-200',
+                'size-4',
+                'group-aria-expanded:rotate-90',
+              )}
+            />
+          ) : (
+            <div className="size-4 min-w-4"></div>
+          )}
           <div
-            key={item.getId()}
-            onDoubleClick={(e) => {
-              onClick(e);
-              props.onDoubleClick?.();
-              props.onSelect?.();
-            }}
-            onClick={() => {
-              tree.setSelectedItems([item.getItemMeta().itemId]);
-              props.onSelect?.();
-            }}
-            {...rowAttrs}
-            {...props}
-            style={{ paddingLeft: `${level * 16}px` }}
             className={cn(
-              'group',
-              'treeitem',
-              'relative',
-              'transition-colors',
-              'flex items-center gap-1',
-              'text-sm',
-              'cursor-pointer',
-              'select-none',
-              'text-foreground',
-              'hover:bg-accent',
-              'aria-selected:bg-selection',
-              'aria-selected:hover:bg-selection',
-              'h-6',
+              'relative flex items-center [&_svg]:size-4',
+              isRoot && node?.data?.loading ? 'animate-spin duration-2000' : '',
             )}
           >
-            {item.isFolder() ? (
-              <ChevronRight
-                onClick={(e) => {
-                  onClick?.(e);
-                  item.onSelect?.();
-                }}
-                className={cn(
-                  'text-foreground-muted',
-                  'transition-transform duration-200',
-                  'size-4',
-                  'group-aria-expanded:rotate-90',
-                )}
-              />
-            ) : (
-              <div className="size-4 min-w-4"></div>
-            )}
-            <div
-              className={cn(
-                'relative flex items-center [&_svg]:size-4',
-              isRoot && node?.data?.loading ? 'animate-spin duration-2000' : '',
-              )}
-            >
-              {getTypeIcon(icon)}
-            </div>
-            <DelayedTooltip content={path}>
-              <div className="truncate font-mono">{displayName ?? name}</div>
-            </DelayedTooltip>
+            {getTypeIcon(icon)}
           </div>
-        </ContextNode>
-      </div>
-    );
+          <DelayedTooltip content={path}>
+            <div className="truncate font-mono">{displayName ?? name}</div>
+          </DelayedTooltip>
+        </div>
+      </ContextNode>
+    </div>
+  );
 };
 
 const Inner = forwardRef<
@@ -224,132 +224,132 @@ export function TreeViewInner({
   ref,
 }: TreeViewInnerProps) {
   const virtualizer = useRef<Virtualizer<HTMLDivElement, Element> | null>(null);
-    const [state, setState] = useState({});
+  const [state, setState] = useState({});
 
-    const customClickBehavior: FeatureImplementation = {
-      itemInstance: {
-        getProps: ({ tree, item, prev }) => ({
-          ...prev?.(),
-          onDoubleClick: (_e: MouseEvent) => {
-            onDoubleClickNode(item);
-            item.primaryAction();
+  const customClickBehavior: FeatureImplementation = {
+    itemInstance: {
+      getProps: ({ tree, item, prev }) => ({
+        ...prev?.(),
+        onDoubleClick: (_e: MouseEvent) => {
+          onDoubleClickNode(item);
+          item.primaryAction();
 
-            if (!item.isFolder()) {
-              return;
-            }
+          if (!item.isFolder()) {
+            return;
+          }
 
-            if (item.isExpanded()) {
-              item.collapse();
-            } else {
-              item.expand();
-            }
-          },
-          onSelect: (_e: MouseEvent) => {
-            onSelectNode(item);
-          },
-        }),
-      },
-    };
+          if (item.isExpanded()) {
+            item.collapse();
+          } else {
+            item.expand();
+          }
+        },
+        onSelect: (_e: MouseEvent) => {
+          onSelectNode(item);
+        },
+      }),
+    },
+  };
 
-    const tree = useTree<Node3Type>({
-      state,
-      setState,
-      rootItemId: ROOT,
-      getItemName: (item) => item.getItemData()?.name,
-      isItemFolder: (item) => !!item.getItemData()?.children,
-      scrollToItem: (item) => {
-        virtualizer.current?.scrollToIndex(item.getItemMeta().index);
-      },
-      dataLoader: {
-        getItem: (id: string) => data[id],
-        getChildren: (id: string) => data[id]?.children ?? [],
-      },
-      features: [
-        syncDataLoaderFeature,
-        selectionFeature,
-        hotkeysCoreFeature,
-        searchFeature,
-        expandAllFeature,
-        customClickBehavior,
-      ],
-    });
+  const tree = useTree<Node3Type>({
+    state,
+    setState,
+    rootItemId: ROOT,
+    getItemName: (item) => item.getItemData()?.name,
+    isItemFolder: (item) => !!item.getItemData()?.children,
+    scrollToItem: (item) => {
+      virtualizer.current?.scrollToIndex(item.getItemMeta().index);
+    },
+    dataLoader: {
+      getItem: (id: string) => data[id],
+      getChildren: (id: string) => data[id]?.children ?? [],
+    },
+    features: [
+      syncDataLoaderFeature,
+      selectionFeature,
+      hotkeysCoreFeature,
+      searchFeature,
+      expandAllFeature,
+      customClickBehavior,
+    ],
+  });
 
-    useEffect(() => {
-      tree.rebuildTree();
-    }, [data]);
+  useEffect(() => {
+    tree.rebuildTree();
+  }, [data]);
 
-    useImperativeHandle(ref, () => tree);
-    return <Inner tree={tree} ref={virtualizer} />;
+  useImperativeHandle(ref, () => tree);
+  return <Inner tree={tree} ref={virtualizer} />;
 }
 
 interface TreeViewProps {
-      dbList: DBType[];
-      search?: string;
+  dbList: DBType[];
+  search?: string;
   ref?: React.Ref<unknown>;
 }
 
 export function TreeView({ dbList, search, ref }: TreeViewProps) {
-    const updateTab = useTabsStore((s) => s.update);
-    const [, setSelectedNode] = useAtom(selectedNodeAtom);
+  const updateTab = useTabsStore((s) => s.update);
+  const [, setSelectedNode] = useAtom(selectedNodeAtom);
 
-    const treeData = useMemo(() => {
-      const _treeData = {
-        id: ROOT,
-        children: dbList.map((db) => ({
-          ...convertId(db.data, db.id, db.displayName),
-          loading: db.loading,
-          icon: db.dialect,
-        })),
-      };
-
-      return convertTreeToMap(
-        filterTree(_treeData as NodeElementType, search) as NodeElementType,
-      );
-    }, [dbList, search]);
-
-    const handleSelectNode = (item: ItemInstance<Node3Type>) => {
-      const itemData = item.getItemData()?.data;
-      setSelectedNode(itemData as unknown as NodeContextType);
+  const treeData = useMemo(() => {
+    const _treeData = {
+      id: ROOT,
+      children: dbList.map((db) => ({
+        ...convertId(db.data, db.id, db.displayName),
+        loading: db.loading,
+        icon: db.dialect,
+      })),
     };
 
-    // BUG: 闭包存在问题，只能获取上一次的外部变量值
-    const handleDoubleClickNode = (item: ItemInstance<Node3Type>) => {
-      const node = item.getItemData()?.data;
-
-      if (!node) {
-        console.warn('doubleClick data is null!');
-        return;
-      }
-
-      const { dbId, path } = node;
-
-      const nodeContext = {
-        dbId,
-        tableId: path as string,
-      };
-
-      const noDataTypes = ['path', 'database', 'root'];
-      if (node && !noDataTypes.includes(node.type ?? '')) {
-        const item: TableContextType = {
-          ...nodeContext,
-          id: node.id,
-          dbId,
-          displayName: node?.name as string,
-          type: 'table',
-        };
-
-        console.log('update tab:', item);
-        updateTab(item);
-      } else {
-        console.warn('doubleClick node is null!');
-      }
-    };
-    return (
-    <TreeViewInner
-        data={treeData}
-        ref={ref}
-        onSelectNode={handleSelectNode}
-        onDoubleClickNode={handleDoubleClickNode}
-      />
+    return convertTreeToMap(
+      filterTree(_treeData as NodeElementType, search) as NodeElementType,
     );
+  }, [dbList, search]);
+
+  const handleSelectNode = (item: ItemInstance<Node3Type>) => {
+    const itemData = item.getItemData()?.data;
+    setSelectedNode(itemData as unknown as NodeContextType);
+  };
+
+  // BUG: 闭包存在问题，只能获取上一次的外部变量值
+  const handleDoubleClickNode = (item: ItemInstance<Node3Type>) => {
+    const node = item.getItemData()?.data;
+
+    if (!node) {
+      console.warn('doubleClick data is null!');
+      return;
+    }
+
+    const { dbId, path } = node;
+
+    const nodeContext = {
+      dbId,
+      tableId: path as string,
+    };
+
+    const noDataTypes = ['path', 'database', 'root'];
+    if (node && !noDataTypes.includes(node.type ?? '')) {
+      const item: TableContextType = {
+        ...nodeContext,
+        id: node.id,
+        dbId,
+        displayName: node?.name as string,
+        type: 'table',
+      };
+
+      console.log('update tab:', item);
+      updateTab(item);
+    } else {
+      console.warn('doubleClick node is null!');
+    }
+  };
+  return (
+    <TreeViewInner
+      data={treeData}
+      ref={ref}
+      onSelectNode={handleSelectNode}
+      onDoubleClickNode={handleDoubleClickNode}
+    />
+  );
 }
